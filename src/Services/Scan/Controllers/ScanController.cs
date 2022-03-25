9c8e5339741daa70ee2268c16abbe11e988236ab
Teneij,Roy H.B <roy.teneij@student.fontys.nl>
2022-03-25 11:55:24 +0100

    Added scan microservice project and cleaned up auto-generated template code

--- src/Services/Scan/Controllers/ScanController.cs
+++ src/Services/Scan/Controllers/ScanController.cs
@@ -1,27 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace User.Controllers
+namespace Scan.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ScanController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<ScanController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public ScanController(ILogger<ScanController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<Scan> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new Scan
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
