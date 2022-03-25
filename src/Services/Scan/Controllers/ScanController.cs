c375915d15dfacb2fb7e1f081101f0a7ac2b1061
Teneij,Roy H.B <roy.teneij@student.fontys.nl>
2022-03-25 14:34:53 +0100

    added firebase hosting workflows

--- src/Services/Scan/Controllers/ScanController.cs
+++ src/Services/Scan/Controllers/ScanController.cs
@@ -6,28 +6,11 @@ namespace Scan.Controllers
     [Route("[controller]")]
     public class ScanController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
         private readonly ILogger<ScanController> _logger;
 
         public ScanController(ILogger<ScanController> logger)
         {
             _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<Scan> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new Scan
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
     }
 }
